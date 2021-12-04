1aaba15a4885965df1979bbfdc197ba4897c48c7
naira_oqni <nairarad2000@gmail.com>
2021-12-05 00:23:32 +0400

    saving record info to DB

--- data_logger.py
+++ data_logger.py
@@ -1,9 +1,11 @@
+import os
 import serial
 import time
 import pandas as pd
 import numpy as np
 import struct
 from PyQt5 import QtGui
+from PyQt5.QtWidgets import QInputDialog
 
 # command <=0, label>0 
 # command codes: start=0, stop=-1, pause=-2, resume=-3, label=1 
@@ -16,12 +18,13 @@ def get_sample_rate(ser):
     return float(1000/timer_interval)
 
 def time_linspace(st_time, sp_time, fs, time_interval):
-    interval = 100/fs
+    interval = 1000/fs
     return np.linspace(st_time, sp_time-interval, int(fs*time_interval)).astype(int)
-
+ 
 def merge_data_and_time(df, df_prot, data_t_l, fs, counter, ind):
-    st=counter; sp=int(counter+fs*df_prot['time_interval'][ind]-1)
-    df.loc[st:sp,'time_millisec'] = time_linspace(data_t_l[ind,0], data_t_l[ind+1,0], fs, df_prot['time_interval'][ind])
+    time_interval = df_prot['time_interval'][ind]
+    st=counter; sp=counter+int(fs*time_interval)-1
+    df.loc[st:sp,'time_millisec'] = time_linspace(data_t_l[ind,0], data_t_l[ind+1,0], fs, time_interval)
     df.loc[st:sp,'label'] = data_t_l[ind][-1]
     counter += int(fs)*df_prot['time_interval'][ind]
     return counter
@@ -30,7 +33,6 @@ def replace_image_by_label(obj_gui, df_prot, indx):
     path_image = df_prot['path_image'][indx]
     obj_gui.motion_pic.setPixmap(QtGui.QPixmap(path_image))
     obj_gui.motion_pic.repaint()
-    # print(path_image)
 
 def receive_data(ser, df_protocol, obj_gui):
     channels_opt = 8
@@ -44,7 +46,7 @@ def receive_data(ser, df_protocol, obj_gui):
     
     send_label(ser, "0") # start signal
     replace_image_by_label(obj_gui, df_protocol, 0)
-    print('\nStart recording')
+    print('Start recording')
     preamb = int.from_bytes(ser.read(4), byteorder='little')
     # print(preamb)
     start_time=0; label_id=0;
@@ -71,9 +73,8 @@ def receive_data(ser, df_protocol, obj_gui):
     data_opt = np.zeros((int(duration*fs0), channels_opt+2), dtype=int)
     data_imu = np.zeros((int(duration*fs1), channels_imu+2), dtype=float)
     data_time_label = np.zeros((df_protocol.shape[0], 2), dtype=int)
-    # print(data_opt.shape, data_imu.shape, data_time_label.shape)
 
-    counter1=0; counter2=0; count_lab=0; lab_ind=0;   #counter0=0;
+    counter1=0; counter2=0; count_lab=0; lab_ind=0; 
     send_label(ser, str(labels[lab_ind]))
     replace_image_by_label(obj_gui, df_protocol, lab_ind)
     lab_ind += 1
@@ -98,27 +99,27 @@ def receive_data(ser, df_protocol, obj_gui):
             elif timer_id==7: 
                 time_millisec = int(int.from_bytes(ser.read(8), byteorder='little')/1000)
                 time_millisec = time_millisec-start_time
-                print('times: {}, counter1 {}, counter2 {}'.format(time_millisec, counter1-1, counter2-1))
+                print('times: {}, counter1 {}, counter2 {}'.format(time_millisec, counter1, counter2))
                 label_id = int.from_bytes(ser.read(4), byteorder='little')    
-                # print('label: ', label_id)
                 data_time_label[lab_ind-1, :] = [time_millisec, label_id]
 
-            if count_lab>=label_interval[lab_ind]*fs0:                
+            if count_lab>=label_interval[lab_ind-1]*fs0:
                 send_label(ser, str(labels[lab_ind]))
                 # print('lab_ind:', lab_ind)
                 replace_image_by_label(obj_gui, df_protocol, lab_ind)
                 lab_ind += 1
                 count_lab=0
 
-        if lab_ind>=df_protocol.shape[0]: #=len(labels):
+        if lab_ind>=df_protocol.shape[0]: 
             send_label(ser, "-1") # stop signal
             replace_image_by_label(obj_gui, df_protocol, 0)
-            time_millisec = time_millisec+(time_millisec-data_time_label[lab_ind-3, 0])
+            interval = 1000/fs0
+            time_millisec = time_millisec+int(interval*fs0*label_interval[lab_ind-1])
+            print('times: {}, counter1 {}, counter2 {}'.format(time_millisec, counter1, counter2))
             data_time_label[lab_ind-1, :] = [time_millisec, label_id]             
             sample = False          
 
     #-------------------
-    # print(data_time_label)
     df_data_opt = pd.DataFrame(data_opt, columns = ['time_millisec','ch11','ch12','ch13','ch14','ch21','ch22','ch23','ch24', 'label'])
     df_data_imu = pd.DataFrame(data_imu, columns = ['time_millisec','Ax','Ay','Az','Gx','Gy','Gz','Mx','My','Mz', 'label'])
     contr=0; contr2=0;
@@ -134,6 +135,44 @@ def dump_data(df_data_opt, df_data_imu, obj_gui):
     df_data_opt.to_csv(obj_gui.path_opt, index=False)
     df_data_imu.to_csv(obj_gui.path_imu, index=False)
     print('data saved')
+
+def save_recording_info_to_db(obj_gui):
+    path_db = obj_gui.edt_db_path.text()+'DataBase.xlsx'
+    print('path_db:',path_db)
+    
+    xls = pd.ExcelFile(path_db)
+    frames = dict(zip(xls.sheet_names,range(len(xls.sheet_names))))
+    if os.path.isfile(path_db) and os.access(path_db, os.R_OK):
+        df=[]
+        for sheet, frame in frames.items(): 
+            df_cur= pd.read_excel(path_db, sheet_name=sheet, dtype={'subject': str})
+            df.append(df_cur)    
+        
+        if len(df[0][df[0]['subject']==obj_gui.edt_patient.text()]['subject_name'])==0:
+            df[0].loc[df[0].shape[0]] = [obj_gui.edt_patient.text(), 'unknown']
+        
+        path_info = obj_gui.path_opt.split('\\')
+        # print( )   
+        subject = obj_gui.edt_patient.text()
+        trial_dir = path_info[-3]
+        cur_dir = path_info[-2]
+        record_name = (path_info[-1])[:-8] 
+        protocol = obj_gui.combo_exp.currentText()
+        place_opt = obj_gui.combo_opt.currentText()
+        place_imu = obj_gui.combo_imu.currentText()
+        limb = obj_gui.combo_limb.currentText()
+        df[-1].loc[df[-1].shape[0]] = [subject,  trial_dir, cur_dir, record_name, place_opt, place_imu, protocol, limb]
+        
+        try:  
+            writer = pd.ExcelWriter(path_db, engine='xlsxwriter')
+            for sheet, frame in frames.items(): 
+                df[frame].to_excel(writer, sheet_name = sheet, index=False)
+            writer.save() 
+            print('db saved')  
+        except OSError:
+            print('File is still open.')
+    else:
+        print('File is not exist.')
 
 def collect_data(obj_gui):
     df_protocol = pd.read_csv('exp_protocols/'+obj_gui.combo_exp.currentText()+'.csv') 
